bbed59fc781b683e687bca6f3852d6affc5177d9
yubiao <literyu@foxmail.com>
2019-07-29 19:25:22 +0800

    nio  请求

--- src/main/java/com/wy/yubiao/nio/NioClient.java
+++ src/main/java/com/wy/yubiao/nio/NioClient.java
@@ -18,27 +18,37 @@ public class NioClient {
     public static void main(String[] args) throws IOException {
         SocketChannel socket = SocketChannel.open();
         socket.configureBlocking(false);//默认阻塞是开启的
-        socket.connect(new InetSocketAddress("127.0.0.1",8083));
+        socket.connect(new InetSocketAddress("www.baidu.com",80));
         //没有完成连接,一直等待
         while (!socket.finishConnect()){
             Thread.yield();
         }
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入:");
-        String s = scanner.nextLine();
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入:");*/
+        String s = "GET / HTTP/1.1\n\rHost:www.baidu.com\n\r\n";
         ByteBuffer buffer = ByteBuffer.wrap(s.getBytes());
         while (buffer.hasRemaining()){
             socket.write(buffer);
         }
         System.out.println("收到服务器响应");
         ByteBuffer buffer1 = ByteBuffer.allocate(1024);
-        while (socket.isOpen() && socket.read(buffer1) != -1){
-            if (buffer1.position() > 0)break;
+        while ( socket.read(buffer1) != -1){
+            buffer1.flip();
+            if(buffer1.limit() > 0) {
+                System.out.println(new String(buffer1.array()));
+            }
+            buffer1.clear();
         }
-        buffer1.flip();
+        /*buffer1.flip();
         byte[] bytes = new byte[buffer1.limit()];
         buffer1.get(bytes);
         System.out.println(new String(bytes));
+        String ss = new String(bytes);
+        String[] split = ss.split("\r");
+        System.out.println(split.length);
+        for (String s1 : split) {
+            System.out.println(s1);
+        }*/
         socket.close();
     }
 }
